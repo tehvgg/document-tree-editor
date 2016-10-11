debf8e46013404a30ca69b5007fcb011863cee9d
Patrick McGuckin <patrick@gskinner.com>
2016-10-11 09:21:30 -0600

    Migrating files from JSFiddle.

---
+++ index.js
@@ -0,0 +1,254 @@
+/**
+ * author / Patrick McGuckin
+ * twitter / @pmcgooks
+ * github / tehvgg
+ */
+
+/**
+ * Prefixes for branches in the tree.
+ * @property prefix
+ */
+const prefix = {
+  node: '|--',
+  node_last: '\\--',
+  spacer_line: '|   ',
+  spacer_empty: '    '
+};
+
+/**
+ * Getters return new HTML elements for injection into the editor.
+ * @property newNode
+ */
+const newNode = {
+  get ul () {
+    return document.createElement('ul');
+  },
+  get li () {
+    return document.createElement('li');
+  },
+  get input () {
+    let el = document.createElement('input');
+    el.type = 'text';
+    el.placeholder = 'Directory or File Name';
+    return el;
+  }
+};
+
+/**
+ * Getters return an array of all matching elements in the editor.
+ * @property domNodes
+ */
+const domNodes = {
+  get input () {
+    return nodeListToArray(document.querySelectorAll('#editor input'));
+  }
+};
+
+// Element storage for quick access.
+const editor = document.querySelector('#editor');
+const btn = document.querySelector('#save');
+const ntf = document.querySelector('.notification');
+const output = document.querySelector('#output');
+const listEntry = document.querySelector('#listEntry');
+const options = {
+  el: document.querySelector('#options'),
+  addSibling: document.querySelector('.addSibling'),
+  addChild: document.querySelector('.addChild'),
+  deleteBranch: document.querySelector('.deleteBranch')
+};
+
+// Global application variables.
+let timeout, activeBranch;
+
+/**
+ * Converts a NodeList to an Array and returns it.
+ * @method nodeListToArray
+ * @returns Array
+ */
+function nodeListToArray (list) {
+  return Array.prototype.slice.call(list);
+}
+
+/**
+ * Saves a string representation of the editor tree to the clipboard.
+ * Requires a browser compatible with the execCommand() API.
+ * @method saveToClipboard
+ */
+function saveToClipboard () {
+  // the range node must be in the DOM to work, this output element is [display: none];
+  output.textContent = treeToString();
+  let range = document.createRange();
+  range.selectNode(output);
+  window.getSelection().addRange(range);
+  try {
+    document.execCommand('copy');
+    ntf.className = 'notification success';
+  } catch (e) {
+    ntf.className = 'notification failed';
+  }
+  // display the notification for 3 seconds
+  setTimeout(() => ntf.className = 'notification', 3000);
+  window.getSelection().removeAllRanges();
+}
+
+/**
+ * Converts the editor tree to string format.
+ * @method treeToString
+ * @returns String
+ */
+function treeToString () {
+  let root = document.querySelector('#root');
+  let tree = root.value || root.placeholder;
+  prefixer(listEntry, 0, function (item, prefix) {
+    let input = item.firstElementChild;
+    tree += `\n${prefix} ${input.value || input.placeholder}`;
+  });
+  return tree.trim();
+}
+
+/**
+ * Applies necessary prefixes to the list items in the editor on update.
+ * @method renderEditor
+ */
+function renderEditor () {
+  prefixer(listEntry, 0, function (item, prefix) {
+    item.dataset.prefix = prefix;
+  });
+}
+
+/**
+ * Recursively determines prefixes for each line in the tree.
+ * 'processor()' is called for each line, handled by the calling method.
+ * @method prefixer
+ */
+function prefixer (activeList, depth, processor, spacers = []) {
+  let items = nodeListToArray(activeList.children);
+  for (let i = 0, l = items.length; i < l; i++) {
+    let last = i === l - 1;
+    let activeItem = items[i];
+    processor(activeItem, spacers.join('') + (last ? prefix.node_last : prefix.node));
+    let nextList = activeItem.lastElementChild;
+    if (nextList.tagName === 'UL') {
+      let arr = spacers.slice();
+      arr.push(last ? prefix.spacer_empty : prefix.spacer_line);
+      prefixer(nextList, depth + 1, processor, arr);
+    }
+  }
+}
+
+function parseDroppedFolder (event) {
+	event.preventDefault();
+  let data = event.dataTransfer;
+  let { files, items } = data;
+  console.log(files);
+  console.log(items);
+}
+
+function toggleEditorFade (event) {
+	editor.className = event.type === 'dragstart' ? 'fade' : '';
+}
+
+/**
+ * Each time a user hovers an input, display the context menu to the right of the element.
+ * @method positionContextMenu
+ */
+function positionContextMenu ({ currentTarget }) {
+  cancelTimeout();
+  activeBranch = currentTarget.parentNode;
+  // Don't allow adding siblings or deleting the root.
+  if (currentTarget.id === 'root') {
+    options.addSibling.style.display = 'none';
+    options.deleteBranch.style.display = 'none';
+  } else {
+    options.addSibling.style.display = null;
+    options.deleteBranch.style.display = null;
+  }
+  let rect = currentTarget.getBoundingClientRect();
+  options.el.className = 'active';
+  options.el.style.left = `${rect.right + 5}px`;
+  options.el.style.top = `${rect.top + scrollY}px`;
+}
+
+/**
+ * Hide the context menu after 1 second.
+ * @method hideContextMenu
+ */
+function hideContextMenu () {
+  timeout = setTimeout(() => {
+    options.el.className = '';
+    activeBranch = undefined;
+  }, 1000);
+}
+
+/**
+ * Add a sibling branch to the active branch.
+ * @method addSibling
+ */
+function addSibling () {
+  let ul = activeBranch.parentNode;
+  let li = newNode.li;
+  li.appendChild(newNode.input);
+  ul.appendChild(li);
+  update();
+}
+
+/**
+ * Add a child branch to the active branch.
+ * @method addChild
+ */
+function addChild () {
+  let ul = activeBranch.lastElementChild;
+  if (ul.tagName !== 'UL') {
+    activeBranch.appendChild(ul = newNode.ul);
+  }
+  let li = newNode.li;
+  li.appendChild(newNode.input);
+  ul.appendChild(li);
+  update();
+}
+
+/**
+ * Delete the active branch and all of its children.
+ * @method deleteBranch
+ */
+function deleteBranch () {
+  activeBranch.parentNode.removeChild(activeBranch);
+  options.el.className = ''; // immediately hide menu.
+  update();
+}
+
+/**
+ * Clear the hide-menu timeout.
+ * @method cancelTimeout
+ */
+function cancelTimeout () {
+  clearTimeout(timeout);
+}
+
+/**
+ * Remove and reapply hover listeners to all input elements and re-render the editor.
+ * Called any time the context menu is used.
+ * @method update
+ */
+function update () {
+  domNodes.input.forEach(node => {
+    if (node.dataset.hasListeners) { return; }
+    node.addEventListener('mouseover', positionContextMenu);
+    node.addEventListener('mouseout', hideContextMenu);
+    node.dataset.hasListeners = true;
+  });
+  renderEditor();
+}
+
+// apply event listeners to the save button and context menu
+btn.addEventListener('click', saveToClipboard);
+options.el.addEventListener('mouseover', cancelTimeout);
+options.el.addEventListener('mouseout', hideContextMenu);
+options.addSibling.addEventListener('click', addSibling);
+options.addChild.addEventListener('click', addChild);
+options.deleteBranch.addEventListener('click', deleteBranch);
+editor.addEventListener('dragenter', toggleEditorFade);
+editor.addEventListener('dragleave', toggleEditorFade);
+editor.addEventListener('drop', parseDroppedFolder);
+// init
+update();
